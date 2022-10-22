f4fb6574e7003ed12248a8292152dc6d2a28271f
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2022-10-22 14:41:39 +0300

    Task - 1

---
+++ js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const addGalleryItem = createGalleryItem(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', addGalleryItem);
+galleryRef.addEventListener("click", onGalleryItemClick);
+
+function createGalleryItem(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => 
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`).join("");
+}
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+    if(event.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`)
+    instance.show();
+
+    galleryRef.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    });
+}
+
